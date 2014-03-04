5c8d9016a7085b649aff239d59e4462763b8f897
good-temper <awayy1432@163.com>
2014-03-04 11:43:40 +0800

    添加StringUtil工具类

---
+++ src/main/java/org/eh/core/util/StringUtil.java
@@ -0,0 +1,13 @@
+package org.eh.core.util;
+
+/**
+ *
+ * @author guojing
+ * @date 2014-3-4
+ */
+public class StringUtil {
+
+	public static boolean isEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
+}
