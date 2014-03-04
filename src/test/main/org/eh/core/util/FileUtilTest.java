48013f545746494bc3b691e2ac017ebb9aef9aaa
good-temper <awayy1432@163.com>
2014-03-04 11:40:56 +0800

    文件读取测试

---
+++ src/test/main/org/eh/core/util/FileUtilTest.java
@@ -0,0 +1,19 @@
+package org.eh.core.util;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author guojing
+ * @date 2014-3-4
+ */
+public class FileUtilTest {
+
+	@Test
+	public void TestRead() {
+		String path = this.getClass().getResource("/").getPath()
+				+ "org/eh/web/view/test/myinfo.page";
+		String content = FileUtil.readFile(path);
+		System.out.println(content);
+	}
+}
